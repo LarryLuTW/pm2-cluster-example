fb6e2e3750d1a014ad310f35e0ee8e11ddcd4734
larry850806 <pudding850806@gmail.com>
2017-11-11 17:46:05 +0800

    Add doSomething in code

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
 
+function doSomething() {
+  Array.from({ length: 100000 }).forEach(value => {
+    return value * value * value
+  })
+}
+
 app.get('/', (req, res) => {
   setTimeout(() => {
+    doSomething()
     res.send('Hello World')
   }, 300)
 })
